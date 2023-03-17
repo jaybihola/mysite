231d0ed5aba31e253433611c31984970c328b451
Jay Bihola <jaybih@outlook.com>
2023-03-16 20:59:52 -0400

    preparation to add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 import { Home } from "./Pages/Home";
-import { ConfigProvider, Layout, theme, Alert } from "antd";
+import { ConfigProvider, Layout, theme, Alert, Menu } from "antd";
 import styled from "styled-components";
 import { Footer } from "./Components/Footer";
+import { menuItems, MenuItems } from "./helpers";
 
-const { Content, Footer: AntdFooter } = Layout;
-
-const AppContainer = styled.div`
-  margin-top: 10vh;
-`;
+const { Content, Footer: AntdFooter, Header: AntdHeader } = Layout;
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const showMenu = false;
   return (
     <ConfigProvider
       theme={{
@@ -20,13 +17,18 @@ function App() {
       }}
     >
       <Layout>
+        <Alert
+          message="This site is still under construction. I am updating it frequently so please check back later for more!"
+          type="warning"
+          showIcon
+          closable
+        />
+        {showMenu && (
+          <AntdHeader>
+            <Menu items={menuItems} mode={"horizontal"} theme="dark" />
+          </AntdHeader>
+        )}
         <Content style={{ padding: `0 10px` }}>
-          <Alert
-            message="This site is still under construction. I am updating it frequently so please check back later for more!"
-            type="warning"
-            showIcon
-            closable
-          />
           <Home />
         </Content>
         <AntdFooter style={{ textAlign: "center" }}>
---
+++ src/helpers.tsx
@@ -0,0 +1,9 @@
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+
+export const menuItems: MenuProps["items"] = [
+  { label: "Home", key: "home" },
+  { label: "About", key: "about" },
+  { label: "Projects", key: "projects" },
+  { label: "Contact", key: "contact" },
+];
